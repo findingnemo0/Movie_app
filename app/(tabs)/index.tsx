b50b4928e34d32ab80f8ebaddd4a17572176f075
Finding_nemo <kamalsingh1472004@gmail.com>
2025-09-28 00:42:25 +0530

    Home screen setup ( early stage )

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,27 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Image, ScrollView, View } from "react-native";
+import { icons, images } from "../../assets";
+
+
 export default function Index() {
   return (
- <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-medium text-red-500">
-        Kamallllllllllllllllllll
-      </Text>
-    </SafeAreaView>
+    <View  className="flex-1 bg-primary">
+      <Image 
+      source={images.bg} 
+      className="absolute w-full z-0"
+      />
+      <ScrollView 
+      className="flex-1 px-5"
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{
+          minHeight:"100%",
+          padding:10,
+      }}
+      >
+          <Image 
+              source={icons.logo}
+              className="w-12 h-10 mt-20 mb-5 mx-auto"
+          />
+      </ScrollView>
+    </View>
   );
 }
