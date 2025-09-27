0da7b28ced5b15853f653ae01910202d35973d7f
Finding_nemo <kamalsingh1472004@gmail.com>
2025-09-28 00:20:39 +0530

    assets setup , bottom navigation setup

---
+++ assets/index.ts
@@ -0,0 +1,31 @@
+import arrow from "./icons/arrow.png"
+import home from "./icons/home.png"
+import logo from "./icons/logo.png"
+import person from "./icons/person.png"
+import play from "./icons/play.png"
+import save from "./icons/save.png"
+import search from "./icons/search.png"
+import star from "./icons/star.png"
+
+import bg from "./images/bg.png"
+import highlight from "./images/highlight.png"
+import Logo from "./images/Logo.png"
+import rankingGradient from "./images/rankingGradient.png"
+
+export const icons = {
+  arrow,
+  home,
+  logo,
+  person,
+  play,
+  save,
+  search,
+  star,
+};
+
+export const images ={
+    bg,
+    highlight,
+    Logo,
+    rankingGradient,
+}
